e5df5f068101ad10fb143a1aad4159d43f904f9e
uday.singh <uday.singh@NOLAPF2LGFWS043.mshome.net>
2020-09-07 18:28:22 +0530

    commit

--- src/test/java/com/qa/store/test/VehicleStoreTestCases.java
+++ src/test/java/com/qa/store/test/VehicleStoreTestCases.java
@@ -45,7 +45,7 @@ public class VehicleStoreTestCases {
       if(actualTitle.equalsIgnoreCase(expectedTitle))
           System.out.println("Title Matched successfully");
       else
-          System.out.println("Title didn't match");    
+          System.out.println("Title didn't matches");    
    
   }
   
